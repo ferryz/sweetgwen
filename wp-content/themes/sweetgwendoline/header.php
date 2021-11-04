a19c431a2b573933c1ed8f1c0f89e2f4d5ad03b3
Kevin Beyer <info@sweetgwendolinefrenchgin.com>
2021-11-04 19:33:37 +0000

    FB Pixel manual code snippet

--- wp-content/themes/sweetgwendoline/header.php
+++ wp-content/themes/sweetgwendoline/header.php
@@ -17,17 +17,35 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<?php wp_head(); ?>
+
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-6P5VHY2BQM"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-211547141-1"></script>
 	<script>
 	  window.dataLayer = window.dataLayer || [];
 	  function gtag(){dataLayer.push(arguments);}
 	  gtag('js', new Date());
 
-	  gtag('config', 'G-6P5VHY2BQM');
+	  gtag('config', 'UA-211547141-1');
 	</script>
 
-	<?php wp_head(); ?>
+	<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '297189298832574');
+	fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=297189298832574&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
 </head>
 
 <body <?php body_class(); ?>>
